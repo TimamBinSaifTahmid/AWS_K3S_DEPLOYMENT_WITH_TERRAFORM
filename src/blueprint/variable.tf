a9147682d4f590eed1604f8eff428f8e4221156e
TimamBinSaifTahmid <timam@iut-dhaka.edu>
2024-10-25 16:16:25 +0000

    terraform added

---
+++ src/blueprint/variable.tf
@@ -0,0 +1,20 @@
+variable "vpc_name"{
+    type= string
+}
+
+variable "vpc_cidr" {
+    type = string
+}
+variable "availability_zones" {
+    type = list(string)
+}
+variable "public_subnets" {
+    type = list(string)
+}
+variable "custom_tag"  {
+    type = map()
+    default = {
+    Terraform   = "true"
+    Environment = "dev"
+    }
+}
---
+++ src/envs/dev/main.tf
@@ -0,0 +1,23 @@
+terraform { 
+  cloud { 
+    
+    organization = "Devops_learning_tahmid" 
+
+    workspaces { 
+      name = "tahmid-dev" 
+    } 
+  } 
+}
+module "tahmid_dev"{
+    source="../../blueprint"
+    vpc_cidr = "10.10.0.0/16"
+    vpc_name = "tahmid-dev-vpc"
+    availability_zones = ["ap-southeast-1a"]
+    public_subnets = ["10.0.0.0/24"]
+    custom_tag={
+        env="tahmid_dev"
+        team="app"
+        managed_by_tf=true
+        workspace="tahmid_dev"
+    }
+}
